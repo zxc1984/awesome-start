6f296324d1479b928a67a2ebffc0d96ba6bb4411
Chris Boesch <scboesch@gmail.com>
2013-01-22 17:17:03 +0800

    Start with Angular-seed to get master branch passing tests.

--- test/e2e/scenarios.js
+++ test/e2e/scenarios.js
@@ -5,27 +5,25 @@
 describe('my app', function() {
 
   beforeEach(function() {
-    browser().navigateTo('../../static/index.html');
+    browser().navigateTo('../../app/index.html');
   });
 
 
   it('should automatically redirect to /view1 when location hash/fragment is empty', function() {
-    //expect(browser().location().url()).toBe("/view1");
-    //pause();
+    expect(browser().location().url()).toBe("/view1");
   });
 
 
   describe('view1', function() {
 
     beforeEach(function() {
-      //browser().navigateTo('#/view1');
+      browser().navigateTo('#/view1');
     });
 
 
     it('should render view1 when user navigates to /view1', function() {
-      //expect(element('[ng-view] p:first').text()).
-      //  toMatch(/partial for view 1/);
-      //pause();
+      expect(element('[ng-view] p:first').text()).
+        toMatch(/partial for view 1/);
     });
 
   });
@@ -34,13 +32,13 @@ describe('my app', function() {
   describe('view2', function() {
 
     beforeEach(function() {
-      //browser().navigateTo('#/view2');
+      browser().navigateTo('#/view2');
     });
 
 
     it('should render view2 when user navigates to /view2', function() {
-      //expect(element('[ng-view] p:first').text()).
-      //  toMatch(/partial for view 2/);
+      expect(element('[ng-view] p:first').text()).
+        toMatch(/partial for view 2/);
     });
 
   });
